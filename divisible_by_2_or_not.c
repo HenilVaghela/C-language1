9442e5ddc56244c706bba6809adc529fd3466349
HenilVaghela <vaghelahenil7@gmail.com>
2025-10-24 13:24:05 +0530

    C program to check divisibility by 2 or not
    Implement a C program to check if a number is divisible by 2 or not

---
+++ divisible_by_2_or_not.c
@@ -0,0 +1,17 @@
+//C PROGRAM
+
+//write a program to cheak if a number is divisible by 2 or not.
+
+#include<stdio.h>
+
+int main()
+{
+int x;
+printf("enter a number");
+scanf("%d",&x);
+printf("%d",x % 2 == 0);
+
+return 0;
+
+
+}
