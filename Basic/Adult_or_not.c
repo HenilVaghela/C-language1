4cd3bcefc6d29d5e38df39882b8418b7aa3d9601
Henil Vaghela <vaghelahenil7@gmail.com>
2025-11-02 01:45:34 +0530

    c program to check to adult or not

---
+++ Basic/Adult_or_not.c
@@ -0,0 +1,30 @@
+//C PROGRAM
+
+//write a program to cheak age is adult or not 
+
+
+#include<stdio.h>
+
+int main() 
+{ 
+    int age;
+    printf("enter  age");
+    scanf("%d",&age);
+    
+    if(age>18){
+          printf("adult\n");
+
+    }else{ 
+         printf("not adult\n");
+         
+    }
+
+    
+    
+
+   return 0;
+
+
+
+
+}
